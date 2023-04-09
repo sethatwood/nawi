6890ccca6711cddff2ead62dd4d68ca037381867
Seth Atwood <seth.atwood@twinthread.com>
2023-04-09 00:52:30 -0400

    wip basic working click to place

--- resources/js/Pages/Game/Board.vue
+++ resources/js/Pages/Game/Board.vue
@@ -1,19 +1,69 @@
+<script>
+export default {
+    data() {
+        return {
+            board: Array(8)
+                .fill()
+                .map(() => Array(8).fill(null)),
+            currentPlayer: "Black",
+            players: {
+                Black: {
+                    element: "air",
+                },
+                White: {
+                    element: "water",
+                },
+            },
+        };
+    },
+    methods: {
+        placePiece(rowIndex, cellIndex) {
+            console.log("Clicked cell:", rowIndex, cellIndex);
+
+            // Check if the cell is empty
+            if (this.board[rowIndex][cellIndex] === null) {
+                // Place the current player's piece on the board
+                this.board[rowIndex][cellIndex] = this.currentPlayer;
+                console.log(
+                    "Placing piece:",
+                    this.currentPlayer,
+                    this.players[this.currentPlayer].element
+                );
+
+                // Switch to the other player
+                this.currentPlayer =
+                    this.currentPlayer === "Black" ? "White" : "Black";
+            }
+        },
+    },
+};
+</script>
+
 <template>
-    <div class="game-board">
-      <div class="grid grid-cols-8 gap-1">
-        <div
-          v-for="row in 8"
-          :key="'row-' + row"
-          class="game-row"
-        >
-          <div
-            v-for="col in 8"
-            :key="`cell-${row}-${col}`"
-            class="game-cell"
-          >
-            <!-- Element components will be placed here -->
-          </div>
+    <div>
+        <p class="mb-4">
+            Current player: {{ currentPlayer }} -
+            {{ players[currentPlayer].element }}
+        </p>
+        <div class="grid grid-cols-8 gap-1">
+            <div
+                v-for="(row, rowIndex) in board"
+                :key="rowIndex"
+                class="grid grid-rows-8 gap-1"
+            >
+                <div
+                    v-for="(cell, cellIndex) in row"
+                    :key="cellIndex"
+                    class="w-16 h-16 border border-gray-300 flex items-center justify-center"
+                    @click="placePiece(rowIndex, cellIndex)"
+                >
+                    <img
+                        v-if="cell"
+                        :src="`/images/${cell}-${players[cell].element}.svg`"
+                        class="w-8 h-8"
+                    />
+                </div>
+            </div>
         </div>
-      </div>
     </div>
 </template>
