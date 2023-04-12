960cf44e5731c02bad600c49b0ef8bb2c25dc913
Seth Atwood <seth.atwood@twinthread.com>
2023-04-12 14:20:36 -0400

    fix bg color; change `/game` to `/play`

--- routes/web.php
+++ routes/web.php
@@ -16,24 +16,24 @@ use Inertia\Inertia;
 */
 
 Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
+  return Inertia::render('Welcome', [
+    'canLogin' => Route::has('login'),
+    'canRegister' => Route::has('register'),
+    'laravelVersion' => Application::VERSION,
+    'phpVersion' => PHP_VERSION,
+  ]);
 });
 
 Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified',
+  'auth:sanctum',
+  config('jetstream.auth_session'),
+  'verified',
 ])->group(function () {
-    Route::get('/dashboard', function () {
-        return Inertia::render('Dashboard');
-    })->name('dashboard');
+  Route::get('/dashboard', function () {
+    return Inertia::render('Dashboard');
+  })->name('dashboard');
 });
 
-Route::get('/game', function () {
-    return Inertia::render('Game/Board');
-})->name('game');
+Route::get('/play', function () {
+  return Inertia::render('Play/Board');
+})->name('play');
